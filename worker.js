4c2dc74167e058ec6c80155382a9e718e65d294f
waradin <kuajiyoshi@gmail.com>
2019-01-23 00:30:09 +0900

    importScriptsのパス指定を変更

--- worker.js
+++ worker.js
@@ -1,7 +1,7 @@
 self.addEventListener('message', function(e){
-  importScripts(e.data.url+'/sha.js');
-  importScripts(e.data.url+'/sha256.js');
-  importScripts(e.data.url+'/mining.js');
+  importScripts('sha.js');
+  importScripts('sha256.js');
+  importScripts('mining.js');
   let hash="";
   let nonce=0;
   do{
